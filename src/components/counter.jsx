7e960503ac6c4488c3064801749f3549b6e2f2c0
AlexLenk <alexlenk.ru@gmail.com>
2022-11-11 07:53:13 +0300

    Added counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
     const imgUrl = 'https://picsum.photos/200'
 
     const formatCount = () => {
@@ -20,6 +21,24 @@ const Counter = () => {
     const handlerDecrement = () => {
         if (!count) return false
         setCount(count - 1)
+    }
+    const handlerTagChange = (id) => {
+        setTags(prevState => prevState.filter(tag => tag !== id))
+        //setTags(['tag4', 'tag5',])
+        console.log(id)
+    }
+
+    const renderTags = () => {
+        if (!tags.length) return 'no tags'
+
+        return tags.map(tag => (
+            <li key={tag} className="btn btn-primary btn-sm m-2"
+                onClick={() => handlerTagChange(tag)}>{tag}</li>
+        ))
+    }
+
+    if (tags.length) {
+        return <ul>{renderTags()}</ul>
     }
 
     return (
